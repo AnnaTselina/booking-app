b010517fcfad5405a8869531e4f4bc871142e879
Anna Celina <anna.celina0000@gmail.com>
2022-10-21 13:07:47 +0300

    init client

---
+++ client/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "plugin:import/typescript", "prettier"],
+  parser: "@typescript-eslint/parser",
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+    typescript: {},
+  },
+  rules: {
+    quotes: [0, "double"],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+    "import/extensions": [
+      "error",
+      "always",
+      {
+        pattern: {
+          jsx: "never",
+          tsx: "never",
+          js: "never",
+        },
+      },
+    ],
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+};
---
+++ client/src/utils/routes.ts
@@ -0,0 +1,9 @@
+const routes = {
+  HOME: "/",
+  SEARCH: "/search",
+  USER: "/user",
+  ADD_RENTAL_UNIT: "/add-rental-unit",
+  APARTMENT: "/apartment",
+};
+
+export default routes;
