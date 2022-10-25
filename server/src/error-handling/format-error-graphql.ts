92daa8086bfb60f65eb48ff36cccea08b68d3f90
Anna Celina <anna.celina0000@gmail.com>
2022-10-26 00:18:20 +0300

    chore: add logging server error

--- server/src/error-handling/format-error-graphql.ts
+++ server/src/error-handling/format-error-graphql.ts
@@ -1,5 +1,8 @@
+import { Logger } from "@nestjs/common";
 import { ApolloError } from "apollo-server-express";
 import { GraphQLError } from "graphql";
+
+const logger = new Logger("NestApplication");
 
 export const codes = {
   internal_server_error: "INTERNAL_SERVER_ERROR",
@@ -17,9 +20,7 @@ export const formatGraphqlError = (error: GraphQLError | ApolloError) => {
     returnedError.message = error.extensions.response.message;
     returnedError.statusCode = error.extensions.response.statusCode;
     returnedError.error = codes.bad_user_input;
-  }
-
-  if (
+  } else if (
     error instanceof GraphQLError &&
     error.extensions.custom &&
     error.extensions.code &&
@@ -28,6 +29,10 @@ export const formatGraphqlError = (error: GraphQLError | ApolloError) => {
     returnedError.message = [error.message];
     returnedError.error = error.extensions.code as string;
     returnedError.statusCode = error.extensions.status as number;
+  } else {
+    logger.error(
+      `${error.message} \n Stack: ${error.extensions.exception?.stacktrace}`,
+    );
   }
 
   return returnedError;
